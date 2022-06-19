f6f12ae91888528bf62fe584a098919eea04d2a6
Luan Daros <darosluan@gmail.com>
2022-06-19 17:23:59 -0300

    build: assets test

--- src/Preload.ts
+++ src/Preload.ts
@@ -1,9 +1,6 @@
 export const Preload = (scene: Phaser.Scene) => {
-  scene.load.image("sky", "/sky.png");
-  scene.load.image("ground", "/platform.png");
-  scene.load.image("star", "/star.png");
-  scene.load.image("bomb", "/bomb.png");
-  scene.load.spritesheet("dude", "/dude.png", {
+  scene.load.image("ground", "/ts-game/platform.png");
+  scene.load.spritesheet("dude", "/ts-game/dude.png", {
     frameWidth: 32,
     frameHeight: 48,
   });
